05050b716358704b28ec200e6f10808c9e3e7394
dera- <michihiro_onodera@dwango.co.jp>
2025-03-10 16:08:34 +0900

    fix resolvePath func (ReftestConfigure)

--- src/configure/ReftestConfigure.ts
+++ src/configure/ReftestConfigure.ts
@@ -166,7 +166,7 @@ export function createReftestConfigure(option: ReftestCommandOption): Normalized
 	const normalizedConfigure: NormalizedReftestConfigure = {
 		...configure,
 		threshold: configure.threshold ?? DEFAULT_IMAGE_DIFF_THRESHOLD,
-		npmCacheDir: configure.npmCacheDir ?? resolvePath(dirPath, "__bincache")
+		npmCacheDir: configure.npmCacheDir ?? resolvePath(dirPath, "__bincache")!
 	};
 	return normalizedConfigure;
 }
@@ -178,8 +178,6 @@ export function resolveTestTypes(type: CommandOptionTestType | null | undefined)
 	);
 }
 
-function resolvePath(driPath: string, targetPath: string): string;
-function resolvePath(driPath: string, targetPath?: string | null): string | null;
-function resolvePath(dirPath: string, targetPath: string | null = null): string | null {
-	return targetPath != null ? path.resolve(dirPath, targetPath) : null;
+function resolvePath(dirPath: string, targetPath: string | null): string | null {
+	return targetPath ? path.resolve(dirPath, targetPath) : null;
 }
