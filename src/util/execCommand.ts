1f20fddc1823367b68cf8ffffd5e0b1780949ab9
dera- <michihiro_onodera@dwango.co.jp>
2025-04-23 13:13:19 +0900

    fix execCommand

--- src/util/execCommand.ts
+++ src/util/execCommand.ts
@@ -4,8 +4,12 @@ export function execCommand(command: string): string {
 	try {
 		return execSync(command).toString();
 	} catch (error: any) {
-		const errorMessage = error.stdout ? error.stdout.toString() : error.message;
-		console.error(errorMessage, error.stderr?.toString()); // 実行環境によってはコンソール上に表示しないとエラー内容が読めない場合もあるため
-		throw new Error(errorMessage);
+		if (error.stdout) {
+			console.log(error.stdout.toString());
+		}
+		if (error.stderr) {
+			console.error(error.stderr.toString());
+		}
+		throw error;
 	}
 }
