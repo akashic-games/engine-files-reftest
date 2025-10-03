9e713b646dd7f544ea4d577c0a33b9dc6bc43c76
dera- <michihiro_onodera@dwango.co.jp>
2025-10-03 15:21:39 +0900

    v3.3.30-beta.2

--- src/scenarioRunner/AppiumServer.ts
+++ src/scenarioRunner/AppiumServer.ts
@@ -1,6 +1,5 @@
 import type { ChildProcess} from "child_process";
 import { exec } from "child_process";
-import * as path from "path";
 import * as getPort from "get-port";
 import fetch from "node-fetch";
 import { untilResolve, withTimeLimit } from "../util/timerUtil";
@@ -27,8 +26,7 @@ export async function createAppiumServer(): Promise<AppiumServerProcess> {
 	// appiumを利用するためにappiumサーバーを先に起動しておく必要がある
 	// appiumもバックグラウンドで起動したままにしておくため非同期で実行する
 	// appium は v1 系でなければならないことに注意。(v2 系には対応できていないことがわかっている)
-	const appiumPath = path.join(__dirname, "../../node_modules/.bin/appium");
-	const process = exec(`${appiumPath} -p ${port}`);
+	const process = exec(`npx --no appium -p ${port}`); // 確実にローカルにインストールされているものを使うため、npx --noを利用
 	// appiumが起動するのを待つ
 	const checkStarting = async (resolve: (value?: unknown) => void, reject: (err?: Error) => void): Promise<void> => {
 		// Appiumが起動していることを確認するためのAPI実行
